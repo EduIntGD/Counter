3a4ca97218bffe098cf9d0dba65eecbd256f9efc
jdgp77 <jdgp77@gmail.com>
2013-12-06 16:04:28 -0500

    JavaWeb

---
+++ src/com/myrtools/counter/Main.java
@@ -0,0 +1,58 @@
+package com.myrtools.counter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/index.html")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+    	//	Tipo de archivo a enviar
+    	response.setContentType("text/html;charset=UTF-8");
+		//	Lugar para poder imprimir
+    	PrintWriter out=response.getWriter();
+    	
+    	try
+    	{
+    		out.print("casa");
+    	}
+    	finally
+    	{
+    		
+    	}
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+}
