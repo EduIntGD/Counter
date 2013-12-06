ada4e353f022c3cfd95a3eec3d45056578b163ab
jdgp77 <jdgp77@gmail.com>
2013-12-06 18:54:33 -0500

    Con el estilo que y las imagenes

--- src/com/myrtools/counter/Main.java
+++ src/com/myrtools/counter/Main.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Main
  */
-@WebServlet("/index.html")
+@WebServlet("/Main.jsp")
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
